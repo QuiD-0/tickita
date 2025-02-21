af459deb0f27d2b00256e0b214be90f80de2a55d
quid-0 <wodnd101@naver.com>
2025-02-21 22:38:06 +0900

    컨트롤러 애니메이션 추가

--- src/renderer/src/components/Controller.vue
+++ src/renderer/src/components/Controller.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="clock__control">
-    <div v-if="isRunning" @click="pause" class="button_box">
-      <div v-if="isPaused">
+    <div v-if="isRunning" @click="pauseTimer" class="button_box">
+      <div v-if="isPaused" id="pause-play">
         <font-awesome-icon icon="fa-solid fa-play"/>
       </div>
-      <div v-else>
+      <div v-else id="pause-pause">
         <font-awesome-icon icon="fa-solid fa-pause"/>
       </div>
     </div>
-    <div v-if="isRunning" @click="stop" class="button_box">
+    <div v-if="isRunning" id="stop" @click="stopTimer" class="button_box">
       <font-awesome-icon icon="fa-solid fa-stop"/>
     </div>
-    <div v-else @click="start" class="button_box">
+    <div v-else @click="startTimer" id="start" class="button_box">
       <font-awesome-icon icon="fa-solid fa-play"/>
     </div>
-    <div class="button_box">
+    <div class="button_box" id="settings">
       <font-awesome-icon icon="fa-solid fa-cog"/>
     </div>
   </div>
 </template>
 
 <script setup>
-import {computed} from 'vue'
+import {computed, onMounted} from 'vue'
 import ControllerStore from "../store/ControllerStore";
 
 const isRunning = computed(() => {
@@ -31,16 +31,56 @@ const isPaused = computed(() => {
   return ControllerStore.getters.isPaused
 })
 
-function start() {
-  ControllerStore.commit('start')
+function startTimer() {
+  const start = document.getElementById('start')
+  const settings = document.getElementById('settings')
+  const pause = document.getElementById('pause-pause')
+  const stop = document.getElementById('stop')
+
+  start.style.transform = 'translateX(-30px)'
+  start.style.opacity = '0'
+  settings.style.transition = 'all 0.4s'
+  settings.style.transform = 'translateX(30px)'
+  setTimeout(() => {
+    settings.style.transition = 'none'
+    settings.style.transform = 'translateX(0px)'
+    settings.style.opacity = '1'
+    ControllerStore.commit('start')
+  }, 400)
+  pause.style.opacity = '1'
+  stop.style.opacity = '1'
 }
 
-function pause() {
+function pauseTimer() {
   ControllerStore.commit('pause')
 }
 
-function stop() {
-  ControllerStore.commit('stop')
+function stopTimer() {
+  const start = document.getElementById('start')
+  const settings = document.getElementById('settings')
+  const pause = document.getElementById('pause-pause')
+  const pausePlay = document.getElementById('pause-play')
+  const stop = document.getElementById('stop')
+
+  settings.style.transition = 'all 0.4s'
+  settings.style.transform = 'translateX(-30px)'
+  if (pause) {
+    pause.style.transition = 'all 0.4s'
+    pause.style.transform = 'translateX(30px)'
+    pause.style.opacity = '0'
+  }
+  if (pausePlay) {
+    pausePlay.style.transition = 'all 0.4s'
+    pausePlay.style.transform = 'translateX(30px)'
+  }
+  stop.style.transition = 'all 0.4s'
+  stop.style.opacity = '0'
+  setTimeout(() => {
+    settings.style.transition = 'none'
+    settings.style.transform = 'translateX(0px)'
+    ControllerStore.commit('stop')
+  }, 400)
+  start.style.opacity = '1'
 }
 </script>
 
